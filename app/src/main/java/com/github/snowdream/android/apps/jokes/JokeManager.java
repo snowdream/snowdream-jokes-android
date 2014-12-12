4af3cd2e276fdb49120e92bc138898cb20a64a33
snowdream <yanghui1986527@gmail.com>
2014-12-13 00:37:34 +0800

    Update: debug ok

--- app/src/main/java/com/github/snowdream/android/apps/jokes/JokeManager.java
+++ app/src/main/java/com/github/snowdream/android/apps/jokes/JokeManager.java
@@ -15,14 +15,14 @@ import java.util.List;
  */
 public class JokeManager {
 
-    public static void getJokes(final CallBack callBack){
+    public static void getJokes(final CallBack callBack) {
         List<Joke> list = null;
         AsyncHttpClient client = new AsyncHttpClient();
         client.get("https://raw.githubusercontent.com/snowdream/snowdream-jokes-android/master/docs/test/jokes.json", new JsonHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-                List<Joke> list =  parseJokes(response);
-                if (callBack != null){
+                List<Joke> list = parseJokes(response);
+                if (callBack != null) {
                     callBack.callback(list);
                 }
             }
@@ -38,10 +38,10 @@ public class JokeManager {
         });
     }
 
-    private static List<Joke> parseJokes(JSONObject obj){
+    private static List<Joke> parseJokes(JSONObject obj) {
         List<Joke> list = null;
 
-        if (obj == null){
+        if (obj == null) {
             return list;
         }
 
@@ -49,26 +49,30 @@ public class JokeManager {
             list = new ArrayList<Joke>();
             JSONArray array = obj.getJSONArray("jokes");
             int length = array.length();
-            for(int i = 0; i < length; i++){
+            for (int i = 0; i < length; i++) {
                 JSONObject oj = array.getJSONObject(i);
 
-                if (oj == null){
+                if (oj == null) {
                     continue;
                 }
                 Joke joke = new Joke();
-                joke.setContent(oj.getString("content"));
-                joke.setAuthor(oj.getString("author"));
-                joke.setCreatetime(oj.getString("createtime"));
-                joke.setUp(oj.getInt("up"));
-                joke.setDown(oj.getInt("down"));
+                joke.setContent(oj.optString("content"));
+                joke.setAuthor(oj.optString("author"));
+                joke.setCreatetime(oj.optString("createtime"));
+                joke.setUp(oj.optInt("up"));
+                joke.setDown(oj.optInt("down"));
 
-                JSONArray images = obj.getJSONArray("images");
-                int size = images.length();
-                List<String> imgs = new ArrayList<String>();
-                for(int j = 0; j < size; j++){
-                    imgs.add(images.getString(j));
+                JSONArray images = oj.optJSONArray("images");
+                if (images != null) {
+                    int size = images.length();
+                    List<String> imgs = new ArrayList<String>();
+                    for (int j = 0; j < size; j++) {
+                        imgs.add(images.getString(j));
+                    }
+                    joke.setImages(imgs);
                 }
-                joke.setImages(imgs);
+
+                list.add(joke);
             }
         } catch (JSONException e) {
             e.printStackTrace();
