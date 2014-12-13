60ca0e5b064d40195147d488c5cfbb8673617127
snowdream <yanghui1986527@gmail.com>
2014-12-14 04:16:32 +0800

    Delete: menu

--- app/src/main/java/com/github/snowdream/android/apps/jokes/MainActivity.java
+++ app/src/main/java/com/github/snowdream/android/apps/jokes/MainActivity.java
@@ -32,45 +32,29 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setData();
     }
 
+    private void setData() {
+        JokeManager.getJokes(new CallBack() {
+            @Override
+            public void callback(List<Joke> jokes) {
+                if (jokes != null) {
+                    googleCardsFragment = new GoogleCardsFragment();
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+                    Bundle bundle = new Bundle();
+                    bundle.putParcelable("jokes",Parcels.wrap(jokes));
+                    googleCardsFragment.setArguments(bundle);
 
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            JokeManager.getJokes(new CallBack() {
-                @Override
-                public void callback(List<Joke> jokes) {
-                    if (jokes != null) {
-                        googleCardsFragment = new GoogleCardsFragment();
-
-                        Bundle bundle = new Bundle();
-                        bundle.putParcelable("jokes",Parcels.wrap(jokes));
-                        googleCardsFragment.setArguments(bundle);
-
-                        //if (savedInstanceState == null) {
-                            getSupportFragmentManager().beginTransaction()
-                                    .add(R.id.container, googleCardsFragment)
-                                    .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
-                                    .commit();
-                       // }
-                    }
+                    //if (savedInstanceState == null) {
+                    getSupportFragmentManager().beginTransaction()
+                            .add(R.id.container, googleCardsFragment)
+                            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                            .commit();
+                    // }
                 }
-            });
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+            }
+        });
     }
  }
